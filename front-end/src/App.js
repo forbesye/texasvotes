b638b4c8e67cf0777ac4c08dea835505bbbe53d2
Jefferson Ye <heroicevil7@gmail.com>
2020-09-20 13:50:52 -0500

    navbar not done yet

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,17 +1,30 @@
 import React from 'react';
-import Navbar from "components/Navbar.js"
+import Navbar from "components/navbar/Navbar.js"
 import {Switch, Route} from "react-router-dom";
 import Splash from "components/Splash.js"
 import About from "components/About.js"
+import { Layout } from 'antd';
+import './App.css'
+
+const { Header, Footer, Content } = Layout; 
 
 const App = () => {
   return (
     <React.Fragment>
-      <Navbar />
-      <Switch>
-        <Route exact path="/" component={Splash} />
-        <Route path="/about" component={About} />
-      </Switch>
+      <Layout className="layout">
+        <Header>
+          <Navbar />
+        </Header>
+        <Content>
+          <Switch>
+            <Route exact path="/" component={Splash} />
+            <Route path="/about" component={About} />
+          </Switch>
+        </Content>
+        <Footer>
+          Footer
+        </Footer>
+      </Layout>
     </React.Fragment>
   );
 }
