f5328f0865e77371495d640538f37a567100996a
Jennifer Suriadinata <jsuriadinata@utexas.edu>
2020-09-23 21:04:03 -0500

    Icons for each about card

--- front-end/src/views/Splash/Splash.js
+++ front-end/src/views/Splash/Splash.js
@@ -2,7 +2,14 @@ import React from 'react';
 import { Button, Row, Col, Card, Typography } from 'antd'
 
 import election_icons from 'views/Splash/images/election_icons.jpg';
-import politician from 'views/Splash/images/politician.svg'
+
+// Icons made by <a href="https://www.flaticon.com/authors/good-ware" title="Good Ware">Good Ware</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a> 
+import texas from 'views/Splash/images/texas.svg'; 
+
+// Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
+import politician from 'views/Splash/images/politician.svg'; 
+import politicians from 'views/Splash/images/politicians.svg'; 
+import vote from 'views/Splash/images/vote.svg';
 
 import styles from './Splash.module.css';
 
@@ -10,47 +17,37 @@ const Splash = () => {
     return(
         <div>
             <div className={styles.flexContainer}>
-                <div style={{marginLeft: "20vw", marginTop: "20vh"}}>
-                    <h1 >
+                <div className={styles.headerText}>
+                    <h1 className={styles.title}>
                         Texas Votes
                     </h1>
-                    <p>
-                        Stay connected and vote!
+                    <p className={styles.about}>
+                        Search for your representatives, your district, and your state and federal elections within the state of Texas!
                     </p>
 
                 </div>
-                <img style= {{marginRight: "20vw", height: "30vh"}}src={election_icons}></img>
+                <img className={styles.headerImage} src={politicians}></img>
             </div>
             <div className={styles.cardFlexContainer}>
                 <Card
                     style={{margin: 15}}
                     cover={<img style={{width: 300, margin: 10}} src={politician}></img>}    
                 >
-                    {/* <Typography.Title level={3}>Card 1</Typography.Title>
-                    hello 1
-                    asdfasdfasdfasdfasdfadsfasdfadsfadsf */}
                     <Button type="primary">Politicians</Button>
                 </Card>
                 <Card
                     style={{margin: 15}}
-                    cover={<img style={{width: 300}}src={election_icons}></img>}    
+                    cover={<img style={{width: 300}}src={texas}></img>}    
                 >
-                    {/* <Typography.Title level={3}>Card 2</Typography.Title>
-                    hello 2
-                    asdfasdfasdfasdfasdfadsfasdfadsfadsf */}
                     <Button type="primary">Districts</Button>
                 </Card>
                 <Card
                     style={{margin: 15}}
-                    cover={<img style={{width: 300}}src={election_icons}></img>}    
+                    cover={<img style={{width: 300}}src={vote}></img>}    
                 >
-                    {/* <Typography.Title level={3}>Card 3</Typography.Title>
-                    hello 3
-                    asdfasdfasdfasdfasdfadsfasdfadsfadsf */}
                     <Button type="primary">Elections</Button>
                 </Card>
             </div>
-            Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
 
         </div>
     );
