3b7e91af27d028689e78782b0063e5922a909c2b
Jennifer Suriadinata <jsuriadinata@utexas.edu>
2020-09-23 20:17:13 -0500

    Basic splash page

--- front-end/src/views/Splash/Splash.js
+++ front-end/src/views/Splash/Splash.js
@@ -1,9 +1,57 @@
 import React from 'react';
+import { Button, Row, Col, Card, Typography } from 'antd'
+
+import election_icons from 'views/Splash/images/election_icons.jpg';
+import politician from 'views/Splash/images/politician.svg'
+
+import styles from './Splash.module.css';
 
 const Splash = () => {
     return(
         <div>
-            Splash page
+            <div className={styles.flexContainer}>
+                <div style={{marginLeft: "20vw", marginTop: "20vh"}}>
+                    <h1 >
+                        Texas Votes
+                    </h1>
+                    <p>
+                        Stay connected and vote!
+                    </p>
+
+                </div>
+                <img style= {{marginRight: "20vw", height: "30vh"}}src={election_icons}></img>
+            </div>
+            <div className={styles.cardFlexContainer}>
+                <Card
+                    style={{margin: 15}}
+                    cover={<img style={{width: 300, margin: 10}} src={politician}></img>}    
+                >
+                    {/* <Typography.Title level={3}>Card 1</Typography.Title>
+                    hello 1
+                    asdfasdfasdfasdfasdfadsfasdfadsfadsf */}
+                    <Button type="primary">Politicians</Button>
+                </Card>
+                <Card
+                    style={{margin: 15}}
+                    cover={<img style={{width: 300}}src={election_icons}></img>}    
+                >
+                    {/* <Typography.Title level={3}>Card 2</Typography.Title>
+                    hello 2
+                    asdfasdfasdfasdfasdfadsfasdfadsfadsf */}
+                    <Button type="primary">Districts</Button>
+                </Card>
+                <Card
+                    style={{margin: 15}}
+                    cover={<img style={{width: 300}}src={election_icons}></img>}    
+                >
+                    {/* <Typography.Title level={3}>Card 3</Typography.Title>
+                    hello 3
+                    asdfasdfasdfasdfasdfadsfasdfadsfadsf */}
+                    <Button type="primary">Elections</Button>
+                </Card>
+            </div>
+            Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
+
         </div>
     );
 }
