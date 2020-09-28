ae06991bcff0a175f3510a49556745ba3a8a2d8a
Jefferson Ye <heroicevil7@gmail.com>
2020-09-28 12:17:22 -0500

    added skeleton files

--- front-end/src/views/Districts/Districts.js
+++ front-end/src/views/Districts/Districts.js
@@ -1,4 +1,6 @@
-import React from "react"
+import { React, useState, usEffect } from "react"
+import { Typography, Divider, Tabs, Input } from "antd"
+import { Route, useLocation, useHistory } from "react-router-dom"
 
 export default function Districts () {
     return <div>Districts go here.</div>
