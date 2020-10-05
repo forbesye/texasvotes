95442dfd4c6d03c9f37e843b144919568d8c1d18
ivanromero321 <ivanromero1000@gmail.com>
2020-10-05 17:30:01 -0500

    Added a route

--- back-end/app.py
+++ back-end/app.py
@@ -1,6 +1,9 @@
 from flask import Flask
 #from dotenv import load_dotenv
+from flask import request, make_response
 import os
+import requests
+import json
 #from sqlalchemy import create_engine
 # import mysql.connector
 # from flask_sqlalchemy import SQLAlchemy
@@ -22,10 +25,28 @@ application = app # This is for AWS Elastic Beanstalk, pls don't remove!!!
 
 # going to start making routes
 
+@app.route('/politician', methods=['GET'])
+def politicians():
+    print(request.query_string)
+    name = request.args.get('name')
+    party = request.args.get('party')
+    district = request.args.get('district')
+    current_office = request.args.get('current_office')
+    incubment = request.args.get('incubment')
+    # print(name)
+    # print(party)
+    # print(district)
+    # print(current_office)
+    # print(incubment)
+    # l = {name, party, district, current_office, incubment}
+    # print(l)
+    # return name  
+
 @app.route('/')
 def hello_world():
     return 'Hello World 2.0!'
 
 if __name__ == '__main__':
     #print(os.getenv("AWS_DEV_DB"))
-    app.run(port=8080, debug=True)
+    # app.run(port=8080, debug=True)
+    app.run(host = "0.0.0.0", port = 5000, debug = True)
