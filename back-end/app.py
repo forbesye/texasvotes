15fcdedc7fa583b6280c65fcd11203c7157e0f30
Sydney <seowen@utexas.edu>
2020-10-05 18:11:04 -0500

    Added test json to politician/id response

--- back-end/app.py
+++ back-end/app.py
@@ -25,6 +25,17 @@ application = app # This is for AWS Elastic Beanstalk, pls don't remove!!!
 
 # going to start making routes
 
+politician_test_json = {"id": 0,"name": "Roger Williams","elected": True,"party": "R","terms": {"current": 4,"total": 5},
+        "offices": {"current": "us_house","past": ["tx_secretary_of_state"]},
+        "district": {"name": "TX-25","id": 1},
+        "biography": "Roger Williams represents the 25th congressional district of Texas which stretches from Tarrant County in the north to Hays County in the south and includes much of the Texas Hill Country and Austin.",
+        "image": "https://williams.house.gov/sites/williams.house.gov/files/wysiwyg_uploaded/CRW%20Official_0_0.jpg",
+        "socials": {"facebook": "http://www.facebook.com/RepRogerWilliams","twitter": "http://twitter.com/RepRWilliams",
+        "youtube": "http://www.youtube.com/channel/UCBtfmMMQarjtLB9U_pWMOhw"},
+        "website": "https://williams.house.gov/","elections": {"upcoming": {"id": 1},"past": [{"id": 2},{"id": 3},{"id": 4},{"id": 5}]},
+        "fundraising": {"raised": 1501689,"spent": 1156825,"remaining_cash": 1268552,"contributors": [{"type": "small_individual",
+        "amount": 10407},{"type": "large_individual","amount": 331010},{"type": "pac","amount": 535343},{"type": "other","amount": 625289}]}}
+
 @app.route('/politician', methods=['GET'])
 def politicians():
     name = request.args.get('name')
@@ -40,7 +51,10 @@ def politicians():
 
 @app.route('/politician/<int:id>', methods=['GET'])
 def politician_id(id):
-    return str(id)
+    if id == 0:
+        return politician_test_json
+    else:
+        return make_response("Error: Politician not found", 404)
 
 @app.route('/district', methods=['GET'])
 def districts():
