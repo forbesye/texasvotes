e735f0d147f78a3d15d1b2b663cb4f6f0330beaa
Jennifer Suriadinata <jsuriadinata@utexas.edu>
2020-10-19 18:43:56 -0500

    Fix pagination and react-player save

--- front-end/src/views/Politicians/GridView.js
+++ front-end/src/views/Politicians/GridView.js
@@ -4,14 +4,11 @@ import { Link } from "react-router-dom"
 import styles from "./Politicians.module.css"
 import { description } from "./Lib"
 import { getAPI } from "library/APIClient"
-import Spinner from "components/ui/Spinner"
 
 const { Title, Paragraph, Text } = Typography
 const { Meta } = Card
 
 export default function GridView () {
-    // const [items, setItems] = useState(politicians)
-    const [loaded, setLoaded] = useState(false);
     const [gridData, setGridData] = useState([]);
     const [currPage, setCurrPage] = useState(1);
     const [total, setTotal] = useState(20);
@@ -19,13 +16,11 @@ export default function GridView () {
 
     const handlePaginationChange = (page) => {
         setCurrPage(page);
-        console.log(page)
         window.scrollTo({top: gridRef.current.offsetTop - 30, behavior: 'smooth'})  
     }
 
     useEffect(() => {
         const fetchData = async () => {
-            setLoaded(false);
             const { page, count } = await getAPI({
                     model: "politician",
                     params: {
@@ -34,50 +29,41 @@ export default function GridView () {
             });
             setTotal(count);
             setGridData(page);
-            setLoaded(true);
         }
         fetchData()
     }, [currPage]);
 
-    let content = null
-    if (loaded) {
-        content = (
-            <Fragment>
-                <section className={styles.content}>
-                    <Title level={2}>View All</Title>
-                    <Paragraph style={{fontSize: 18}}>Have you ever wondered what all Texas officials and candidates look like in a grid view? Probably not, but we've got you covered here. The grid can also be filtered and sorted by different properties to make your viewing experience more customizable (soon™).</Paragraph>
-                </section>
-                <Divider />
-                <section className={styles.grid} ref={gridRef}>
-                    { gridData.map((item, i) => (
-                        <Link to={`/politicians/view/${item.id}`}>
-                            <Card
-                                className={item.party === "R" ? styles.cardRep : styles.cardDem}
-                                hoverable
-                                cover={<img className={styles.croppedImage} alt={item.name} src={item.image} />}
-                            >
-                                <Meta 
-                                    title={<Text style={{fontSize: 20}}>{item.name}</Text>}
-                                    description={<Text style={{fontSize: 18}}>{description(item)}</Text>}
-                                />
-                            </Card>
-                        </Link>
-                    ))}
-                </section>
-                <Pagination
-                    total={total}
-                    defaultCurrent={1}
-                    defaultPageSize={20}
-                    onChange={handlePaginationChange}
-                    pageSizeOptions={[]}
-                    style={{margin: "16px 0", display: "flex", justifyContent:"flex-end"}}
-                />
-            </Fragment>
-        )
-    }
     return (
-        <div>
-            { loaded ? content : <Spinner /> }
-        </div>
+        <Fragment>
+            <section className={styles.content}>
+                <Title level={2}>View All</Title>
+                <Paragraph style={{fontSize: 18}}>Have you ever wondered what all Texas officials and candidates look like in a grid view? Probably not, but we've got you covered here. The grid can also be filtered and sorted by different properties to make your viewing experience more customizable (soon™).</Paragraph>
+            </section>
+            <Divider />
+            <section className={styles.grid} ref={gridRef}>
+                { gridData.map((item, i) => (
+                    <Link to={`/politicians/view/${item.id}`}>
+                        <Card
+                            className={item.party === "R" ? styles.cardRep : styles.cardDem}
+                            hoverable
+                            cover={<img className={styles.croppedImage} alt={item.name} src={item.image} />}
+                        >
+                            <Meta 
+                                title={<Text style={{fontSize: 20}}>{item.name}</Text>}
+                                description={<Text style={{fontSize: 18}}>{description(item)}</Text>}
+                            />
+                        </Card>
+                    </Link>
+                ))}
+            </section>
+            <Pagination
+                total={total}
+                defaultCurrent={1}
+                defaultPageSize={20}
+                onChange={handlePaginationChange}
+                pageSizeOptions={[]}
+                style={{margin: "16px 0", display: "flex", justifyContent:"flex-end"}}
+            />
+        </Fragment>
     )
 }
