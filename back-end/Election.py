0896dde5cc17871edc0a64ca3567d098f3fb1149
Sydney <seowen@utexas.edu>
2020-11-03 17:41:23 -0600

    Added searching by candidate to district and election

--- back-end/Election.py
+++ back-end/Election.py
@@ -107,6 +107,7 @@ def search_elections(q, elect_query):
         searches.append(Election.class_name.match(term))
         searches.append(Election.office.match(term))
         searches.append(District.counties.any(name=term))
-    elect_query = elect_query.join(District).filter(or_(*tuple(searches)))
+        searches.append(Politician.name.match(term))
+    elect_query = elect_query.join(District).join(Politician).filter(or_(*tuple(searches)))
 
     return elect_query
