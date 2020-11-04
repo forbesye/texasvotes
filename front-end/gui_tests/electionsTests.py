6150d4839a848938a8c87e3a3032ab2c92b049fb
Jennifer Suriadinata <jsuriadinata@utexas.edu>
2020-11-04 12:03:28 -0600

    Sort and filter tests for elections

--- front-end/gui_tests/electionsTests.py
+++ front-end/gui_tests/electionsTests.py
@@ -1,11 +1,12 @@
 import unittest
 import time
+import sys
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import sys
+from selenium.webdriver.common.action_chains import ActionChains
 
 # PATH = "chromedriver.exe"
 PATH = "./front-end/gui_tests/chromedriver.exe"
@@ -19,6 +20,7 @@ class TestElections(unittest.TestCase):
     def setUpClass(cls):
         cls.driver = webdriver.Chrome(PATH)
         cls.driver.get(URL)
+        cls.actions = ActionChains(cls.driver)
 
     # Close browser and quit after all tests
     @classmethod
@@ -53,6 +55,74 @@ class TestElections(unittest.TestCase):
         self.driver.find_element_by_id("rc-tabs-2-tab-search").click()
         currentURL = self.driver.current_url
         assert currentURL == "https://stage.texasvotes.me/elections/search"
+
+        self.driver.back()
+        element = self.driver.find_element_by_tag_name('h1')
+        assert element.text == 'Texas Elections'
+
+    def testSort1(self):
+        try:
+            a = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.CLASS_NAME, 'Elections_filterSection__1CnPM'))
+            )
+        except Exception as ex:
+            print(ex)
+            return
+
+        selections = self.driver.find_element_by_class_name('Elections_filterSection__1CnPM')
+        selections.find_elements_by_class_name('ant-select')[0].click()
+        time.sleep(2)
+        self.actions.send_keys(Keys.DOWN, Keys.DOWN, Keys.RETURN).perform()
+        time.sleep(2)
+
+        try:
+            a = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.CLASS_NAME, 'ant-table-row'))
+            )
+        except Exception as ex:
+            print("did not find")
+            return
+
+        self.driver.find_elements_by_class_name('ant-table-row')[0].click()
+        time.sleep(2)
+        element = self.driver.find_element_by_class_name('ant-page-header-heading-title')
+        assert element.find_element_by_tag_name('div').text == '2020 Democratic Primary for Texas House of Representatives District 101'
+
+        self.driver.back()
+        element = self.driver.find_element_by_tag_name('h1')
+        assert element.text == 'Texas Elections'
+
+    def testFilter1(self):
+        try:
+            a = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.CLASS_NAME, 'Elections_filterSection__1CnPM'))
+            )
+        except Exception as ex:
+            print(ex)
+            return
+
+        selections = self.driver.find_element_by_class_name('Elections_filterSection__1CnPM')
+        selections.find_elements_by_class_name('ant-select')[1].click()
+        time.sleep(2)
+        self.actions.send_keys(Keys.RETURN, Keys.ESCAPE).perform()
+        time.sleep(2)
+
+        try:
+            a = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.CLASS_NAME, 'ant-table-row'))
+            )
+        except Exception as ex:
+            print("did not find")
+            return
+
+        self.driver.find_elements_by_class_name('ant-table-row')[0].click()
+        time.sleep(2)
+        element = self.driver.find_element_by_class_name('ant-page-header-heading-title')
+        assert element.find_element_by_tag_name('div').text == '2020 General Election for Texas House of Representatives District 8'
+
+        self.driver.back()
+        element = self.driver.find_element_by_tag_name('h1')
+        assert element.text == 'Texas Elections'
 
 if __name__ == "__main__":
     PATH = sys.argv[1]
