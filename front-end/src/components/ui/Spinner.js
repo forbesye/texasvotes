27da8856927a89eda130b90b47c212b8e21d6fa8
Lawrence Win <lawrence.j.win@gmail.com>
2020-11-12 15:55:36 -0600

    Put new spinner messages

--- front-end/src/components/ui/Spinner.js
+++ front-end/src/components/ui/Spinner.js
@@ -2,18 +2,25 @@ import React from "react"
 import { Spin } from "antd"
 
 const messages = [
-	"Looking for the fabled blue wave",
+	"Rip blue wave :(",
 	"Waiting for Ken Paxton to stand trial for securities fraud",
 	"Listening to Dennis Bonnen lose his job as Speaker of the House",
-	"Dreaming of a Biden/Harris upset",
-	"Waiting for Governor Greg Abbott to terrorize liberal cities once more",
-	"Waiting for Lt. Governor Dan Patrick to say something stupid again",
+	"The Joe-mentum came guys",
+	"\"Kraka-Joe-a\"",
+	"Waiting for Greg Abbott to terrorize liberal cities once more",
+	"Waiting for Dan Patrick to say something stupid again",
 	"Attempting to court the Hispanic vote",
 	"Looking for Beto O'Rourke stumping on some tables",
 	"Trying to stop Ted Cruz from reading 'Green Eggs and Ham' on the Senate Floor",
 	"Searching for the Zodiac Killer",
 	"Stalling the USPS",
 	"Brushing off cheeto dust",
+	"STOPPING THE COUNT",
+	"Searching for widespread voter fraud",
+	"Standing back and standing by",
+	"Sending in the poll watchers",
+	"Undermining the democratic transfer of power",
+	"Looking for Ted Cruz's spine"
 ]
 
 const styles = {
